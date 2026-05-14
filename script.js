59599677a88ef2f0de8971cc76baf5ef1d78fb13
evelynhyx <evebabyhua@gmail.com>
2026-05-13 22:03:44 -0400

    base css done, created script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const toDoForm = document.querySelector('form');
+const toDoInput = document.getElementById('to-do-input');
+const toDoListUL = document.getElementById('to-do-list');
+
+let allToDos = [];
